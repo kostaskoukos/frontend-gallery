5e41ab0cf56d20b029db32cb5329ecd19e6302bc
kostaskoukos <kkonst2014@gmail.com>
2025-02-09 17:05:34 +0200

    add alpine astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [alpinejs()],
 });
+
